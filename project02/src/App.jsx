9a571de3ee1b75c4f59aeff8d6e40fdc5a0de158
Soda <k.sua923@gmail.com>
2025-05-13 22:13:23 +0900

    react 개발자도구 추가, todo 등록 기능추가

--- project02/src/App.jsx
+++ project02/src/App.jsx
@@ -1,14 +1,51 @@
 import './App.css'
+import { useState, useRef } from 'react'
+
 import Header from './component/Header'
 import TodoEditor from './component/TodoEditor'
 import TodoList from './component/TodoList'
+//import dummy from './db/mock.json'
+
+const mockTodo=[
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    createDate: new Date().getTime()
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래 널기",
+    createDate: new Date().getTime()
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    createDate: new Date().getTime()
+  }
+];
 
 function App() {
+  const [todo, setTodo]=useState(mockTodo);
+  const idRef=useRef(3);
+
+  const onCreate=(content)=>{
+    const newItem={
+      id: idRef.current,
+      content: content,
+      isDone: false,
+      createDate: new Date().getTime()
+    };
+    setTodo([newItem, ...todo]);
+    idRef.current+=1;
+  };
 
   return (
     <div className='App'>
       <Header />
-      <TodoEditor />
+      <TodoEditor onCreate={onCreate} />
       <TodoList />
     </div>
   )
--- project02/src/component/TodoEditor.jsx
+++ project02/src/component/TodoEditor.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import "./TodoEditor.css";
 
-const TodoEditor=()=>{
+const TodoEditor=({onCreate})=>{
+    const [content, setContent]=useState("");
+    const onChangeContent=(e)=>{
+        setContent(e.target.value);
+    }
+
+    const onSubmit=()=>{
+        onCreate(content);
+    }
+
     return(
         <div className="TodoEditor">
             <h4>새로운 Todo 작성하기 ✏️</h4>
             <div className="editor_wrapper">
-                <input type="text" placeholder="오늘의 할 일은?" />
-                <button>추가</button>
+                <input 
+                    value={content}
+                    onChange={onChangeContent}
+                    //type="text" 
+                    placeholder="오늘의 할 일은?" />
+                <button onClick={onSubmit}>추가</button>
             </div>
         </div>  
     );
